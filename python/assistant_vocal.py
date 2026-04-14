05e3b0426fb6b0d31dc8ce23b53c85cae0e4eb4b
Yasmine <benltaiefyassmine@gmail.com>
2026-04-14 09:37:09 +0100

    ajout liaison rpi arduino

--- python/assistant_vocal.py
+++ python/assistant_vocal.py
@@ -12,7 +12,32 @@ FICHIER_COMMANDE = "data/commande.txt"
 def ecrire_commande(texte):
     with open(FICHIER_COMMANDE, "w", encoding="utf-8") as f:
         f.write(texte + "\n")
+import serial
+import time
 
+arduino = serial.Serial('/dev/ttyACM0', 9600)
+time.sleep(2)
+
+def envoyer_action():
+    with open("data/action.txt", "r") as f:
+        lines = f.readlines()
+
+    if not lines:
+        return
+
+    derniere = lines[-1].strip()
+    print("Action envoyée :", derniere)
+
+    if "advance" in derniere:
+        arduino.write(b'1')
+    elif "retreat" in derniere:
+        arduino.write(b'2')
+    elif "left" in derniere:
+        arduino.write(b'3')
+    elif "right" in derniere:
+        arduino.write(b'4')
+    elif "turn" in derniere:
+        arduino.write(b'5')
 def main():
     r = sr.Recognizer()
 
@@ -34,7 +59,7 @@ def main():
     except sr.RequestError as e:
         print("[ERREUR] Problème API :", e)
         ecrire_commande("")
-
+    envoyer_action()
     print("End!")
 
 if __name__ == "__main__":
